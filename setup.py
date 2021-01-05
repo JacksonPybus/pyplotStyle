607a4f5f99c565e9c5f57280fcea7cc42fd03e64
JacksonPybus <jacksonpybus@gmail.com>
2021-01-05 16:32:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pyplotStyle',
+    name='pystyle',
     version='0.1.0',
     description='A package of plotting functions for pyplot figures',
     long_description=long_description,
